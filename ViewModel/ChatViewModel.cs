c6e39263326dcdc1424962dec6fa74afabe1c9e7
lalli-oni <lalli.oni@gmail.com>
2015-09-14 11:46:25 +0200

    Added a connect button that initiates a connection to a predefined server

--- ViewModel/ChatViewModel.cs
+++ ViewModel/ChatViewModel.cs
@@ -4,6 +4,8 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KirKClient.Facade;
+using KirKClient.Handler;
 
 namespace KirKClient.ViewModel
 {
@@ -11,6 +13,8 @@ namespace KirKClient.ViewModel
     {
         private ObservableCollection<string> _receivedMessages;
         private string _inputMessage;
+        private RelayCommand _connectCommand;
+        private NetworkFacade _netFacade;
 
         public ObservableCollection<string> ReceivedMessages
         {
@@ -24,9 +28,22 @@ namespace KirKClient.ViewModel
             set { _inputMessage = value; }
         }
 
+        public RelayCommand ConnectCommand
+        {
+            get { return _connectCommand; }
+            set { _connectCommand = value; }
+        }
+
         public ChatViewModel()
         {
+            _netFacade = new NetworkFacade();
             _receivedMessages = new ObservableCollection<string>();
+            _connectCommand = new RelayCommand(connectToServer);
+        }
+
+        public void connectToServer()
+        {
+            _netFacade.Connect();
         }
     }
 }
